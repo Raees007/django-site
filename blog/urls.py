71ea10ecd21a322ccf2a3e320d7f6b7ee7b91f6f
Raees <r33857328@gmail.com>
2019-08-03 03:29:30 +0500

    Add feature to comment on blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft, name='post_draft'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
-    # path('accounts/login/', auth_view.LoginView.as_view(), name='login'),
-
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 # Create your views here.
 
 def post_list(request):
@@ -59,6 +59,17 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
-
-
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
